0def63370cdf6e66f34c78829785cf926a0f802a
m-firoozi <firoozimehrnoosh@yahoo.com>
2024-09-21 14:17:54 +0330

    add-footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,10 @@
+import styles from "./footer-style.module.css"
 const Footer=() =>{
+    const d=new Date()
+    let year = d.getFullYear();
     return(
         <>
-        
+        <p className={styles.copyright}>copyright © {year}</p>
         </>
     )
 }
